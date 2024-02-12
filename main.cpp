d93f557928ad1b8902b7f21215fb86df40a9114d
raperez2 <70178601+raperez2@users.noreply.github.com>
2024-02-12 12:05:59 -0800

    Update main.cpp
    Made calculator working

--- main.cpp
+++ main.cpp
@@ -3,14 +3,32 @@ using namespace std;
 
 int main(){
   string method;
+  int firstnum, secnum, result;
   cout<<"Hello World\n";
   cout<<"Here is a basic calculator\n";
   cout<<"Add or Subtract?\n";
-  cin>>method;
+  cin>>method<<endl;
   while(method != "Add" || method != "Subtract" ||method != "add" || method != "subtract"){
     cout<<"Please specify Add or Subtract.\n";
-    cin>>method;
+    cin>>method<<endl;
   }
-  cout<<"To be continued\n";
+  cout<<"What first number?\n";
+  cin>>firstnum<<endl;
+  cout<<method<<"ed to\n";
+  cin>>secnum<<endl;
+  switch(method):
+    case 'Add' || 'add':
+      cout<<"Adding "<<firstnum<<" to "<<secnum<<endl;
+      result = firstnum + secnum;
+      break;
+    case 'Subtract' || 'subtract':
+      cout<<"Subtracting "<<firstnum<<" with "<<secnum<<endl;
+      result = firstnum - secnum;
+      break;
+    default:
+      cout<<"Error unknown method\n";
+      break;
+  cout<<"Your result is "<<result<<endl;
+  cout<<"Exiting...\n";
   return 0;
 }
